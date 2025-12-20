8d413521c4ceff95705fea719f52636c26bd5a43
samosa <moosa.binnaseem09@gmail.com>
2025-12-21 03:38:09 +0500

    added stack model

--- umoses-portfolio.go
+++ umoses-portfolio.go
@@ -4,33 +4,60 @@ import (
 	"fmt"
 	"os"
 	tea "github.com/charmbracelet/bubbletea"
+//	gloss "github.com/charmbracelet/lipgloss"
+//	wish "github.com/charmbracelet/wish"
+//	bubbles "github.com/charmbracelet/bubbles"
+//	harmonica "github.com/charmbracelet/harmonica"
 )
 
+type sessionState int
 const (
+	homeView sessionState = iota
+	stackView
+//	aboutView
+//	projsNexpView 
 	host = "0.0.0.0"
 	port = 23234
 )
 
-type model struct {
-	items    []string
-	cursor   int
-	selected map[int]struct{}
+func main() {
+	p:= tea.NewProgram(initialModel())
+	if _, err := p.Run(); err !=nil {
+		fmt.Printf("There been an error %v", err)
+		os.Exit(1)
+	}
 }
 
-func initialModel() model {
-	return model{
+type home struct {
+	state	sessionState
+	items   []string
+	cursor   int
+	selected map[int]struct{}
+	stack		stack
+	width int 
+	height int
+}
+
+
+
+func initialModel() home {
+	return home{
 		items: []string{"Stack", "Projects", "Experience", "Contact"},
 
 		selected: make(map[int]struct{}),
 	}
 }
 
-func (m model) Init() tea.Cmd {
+//initialize home
+func (m home) Init() tea.Cmd {
 	return nil
 }
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+//update home
+func (m home) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q":
@@ -51,15 +78,23 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			} else {
 				m.selected[m.cursor] = struct{}{}
 			}
-
+		case "s":
+			m.state = stackView
 		}
 
+
+	var cmd tea.Cmd
+	switch m.state {
+	case stackView:
+			m.stack, cmd = m.stack.Update(msg)
 	}
 	return m, nil
 }
 
-func (m model) View() string {
-	s := `[[
+//view home
+func (m home) View() string {
+	
+	homepageString := `
                                                                                                                                           
                              *                  ***                         *****   **    **                                              
                            **                    ***                     ******  ***** *****                                              
@@ -81,7 +116,7 @@ func (m model) View() string {
                                 *                       *      ****                                                                       
                                *                                                                                                          
                                                                                                                                           
-]]
+
 `	
 	for i, item := range m.items{
 		cursor := " "
@@ -92,18 +127,41 @@ func (m model) View() string {
 		checked := " "
 			if _, ok := m.selected[i]; ok {
 			checked = "X"
-		 	}
+		  }
 
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked,item)
+		homepageString += fmt.Sprintf("%s [%s] %s\n", cursor, checked,item)
 		}
-	s+= "\n Press Q to quit."
-	return s
+	homepageString+= "\n Press Q to quit."
+	return homepageString
+}
+//struct for Stack info
+type stack struct{
+	languages []string
 }
 
-func main() {
-	p:= tea.NewProgram(initialModel())
-	if _, err := p.Run(); err !=nil {
-		fmt.Printf("There been an error %v", err)
-		os.Exit(1)
+
+//Initialize Stack
+func (s stack) Init (tea.Cmd) {
+	return nil
+}
+
+
+//Updaate stack
+func (s stack) Update(msg tea.Msg) (tea.Msg, tea.Cmd){
+
+	switch msg := msg.(type){
+	case msg.KeyMsg:
+		switch msg.String(){
+		case "q":
+			return s, tea.Quit
+		}
 	}
+	return s,nil
+}
+
+// ViewStack
+func (s stack) View() string{
+	stackString:= "My stack basically comprises of Python, Rust, Go and C/C++, However, I would classify my strongest suite as python"
+
+	return stackString
 }
